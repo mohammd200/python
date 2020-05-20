2e55da048e05459bf743662587e8fff259380743
mohammd <mohammd_200@hotmail.com>
2020-05-20 04:51:09 +0300

    numbers

---
+++ numbers.py
@@ -0,0 +1,13 @@
+print(20 + 20)
+print(20 - 20 )
+print (5 * 2 )
+print (10 / 2 )
+print(3 ** 10)
+
+num1 = 10.1
+num2 = 20
+num2 = num2 + 1
+num2 +=1
+num3 = num1+num2
+print(num2)
+print(num3)
--- string.py
+++ string.py
@@ -2,5 +2,5 @@ fname = 'mohammd'
 lname ='aljabl'
 full_name = "mohammd aljabl"
 print (full_name.replace(lname,full_name))
-b = 'love' * 500
+b = 'LOVE ALJABAL' * 5
 print(b)
